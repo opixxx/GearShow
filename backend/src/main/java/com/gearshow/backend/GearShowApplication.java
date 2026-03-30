50110f85a8f9d871790b32085dd285082b88d652
opixxx <opix0306@naver.com>
2026-03-30 19:27:03 +0900

    Initial project setup: Spring Boot + Java 21 + Gradle + MySQL

---
+++ backend/src/main/java/com/gearshow/backend/GearShowApplication.java
@@ -0,0 +1,13 @@
+package com.gearshow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearShowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gearshow/backend/GearShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gearshow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
